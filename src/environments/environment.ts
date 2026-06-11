5237b1ac3309258ed6afe28dbf5a55dbfcc08235
Tushar1082 <tusharsharma1082@gmail.com>
2026-06-11 12:03:12 +0530

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://192.168.1.43:3000/'
-    // apiUrl: 'https://testhaemovigilapi.atf-labs.com/'
+    apiUrl: 'https://testhaemovigilapi.atf-labs.com/'
 };
